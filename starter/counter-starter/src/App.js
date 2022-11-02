a0b67929d2b7390e9e60308cc3c5d7a7d45dfeb0
bando9 <bandomega1@gmail.com>
2022-11-02 19:47:05 +0700

    menghapus counter

--- starter/counter-starter/src/App.js
+++ starter/counter-starter/src/App.js
@@ -3,15 +3,12 @@ import { useState } from 'react'
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <h1>{count}</h1>
-
-      <button onClick={() => {setCount (count + 1)}}>Plus</button>
-      <button onClick={() => {setCount (count - 1)}}>Minus</button>
-    </div>
+    <>
+      <nav className="nav">
+        
+      </nav> 
+    </>
   );
 }
 
