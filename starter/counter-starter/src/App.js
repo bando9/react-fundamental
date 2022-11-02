82d4251def480f3802579be3a1eb081b63ec5e16
bando9 <bandomega1@gmail.com>
2022-11-02 20:36:45 +0700

    menambah form input + add-button

--- starter/counter-starter/src/App.js
+++ starter/counter-starter/src/App.js
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 
 import './App.css';
+import shoppingIcon from './assets/shopping-icon.svg';
 
 function App() {
   return (
     <>
       <nav className="nav">
-        
-      </nav> 
+        <img className="nav-icon" src={shoppingIcon} alt="shopping icon"/>
+        <h1 className="nav-title">Shopping List</h1>
+      </nav>
+
+      <section className="container">
+        <form className="form">
+          <input 
+            className="input"
+            type="text"
+            placeholder="List"
+          />
+          <button className="add-button" type="submit">add</button>
+        </form>
+      </section>
     </>
   );
 }
